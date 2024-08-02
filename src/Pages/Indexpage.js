8c249b4edf50c556c200adc7a0033ad791a7c3f9
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-08-02 23:08:27 +0530

    Update Indexpage.js

--- src/Pages/Indexpage.js
+++ src/Pages/Indexpage.js
@@ -1,34 +1,4 @@
-// import {useEffect, useState} from "react";
-// import axios from "axios";
-// import {Link} from "react-router-dom";
-// import Image from "../Image.js";
 
-// export default function Indexpage() {
-//   const [places,setPlaces] = useState([]);
-//   useEffect(() => {
-//     axios.get('/places').then(response => {
-//       setPlaces(response.data);
-//     });
-//   }, []);
-//   return (
-//     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-//       {places.length > 0 && places.map(place => (
-//         <Link to={'/places/'+place._id}>
-//           <div className="bg-gray-500 mb-2 rounded-2xl flex h-60">
-//             {place.photos?.[0] && (
-//               <Image className="rounded-2xl object-cover aspect-square " src={place.photos?.[0]} alt=""/>
-//             )}
-//           </div>
-//           <h2 className="font-bold">{place.address}</h2>
-//           <h3 className="text-sm text-gray-500">{place.title}</h3>
-//           <div className="mt-1">
-//             <span className="font-bold">${place.price}</span> per night
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -88,8 +58,8 @@ export default function Indexpage() {
                 <Image className="rounded-2xl object-cover aspect-square " src={place.photos?.[0]} alt=""/>
               )}
             </div>
-            <h2 className="font-bold">{place.address}</h2>
-            <h3 className="text-sm text-gray-500">{place.title}</h3>
+            <h2 className="font-bold">{place.title}</h2>
+            <h3 className="text-sm text-gray-500">{place.address}</h3>
             <div className="mt-1">
               <span className="font-bold">{place.price > 0 ? `$${place.price}` : "Free "}</span>
                {place.price > 0 ? " per month" : "entry"}
